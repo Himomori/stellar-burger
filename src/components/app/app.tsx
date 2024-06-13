30893e23fa84bfe2dd208f7cb14c7ea8ff6fcea8
Himomori <Himomori@bk.ru>
2024-06-13 14:06:32 +1100

    открытие всех страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,45 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Routes, Route } from 'react-router-dom';
+import { title } from 'process';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      {/* защищенные маршруты отсюда */}
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      {/* до сюда */}
+      {/* модальные окна с доп. информацией */}
+      <Route path='/feed/:number' element={<OrderInfo />} />
+      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+      <Route path='/profile/orders/:number' element={<OrderInfo />} />
+      {/* до сюда */}
+      {/* страница ошибки */}
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
