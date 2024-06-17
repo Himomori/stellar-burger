60058298677d0f35cd63952d9d3c4c3582fc9471
Himomori <Himomori@bk.ru>
2024-06-18 08:45:44 +1100

    отображение ингредиентов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,46 +1,56 @@
-import {
-  ConstructorPage,
-  Feed,
-  ForgotPassword,
-  Login,
-  NotFound404,
-  Profile,
-  ProfileOrders,
-  Register,
-  ResetPassword
-} from '@pages';
+import React, { useEffect } from 'react';
+import { ConstructorPage } from '@pages';
+import { Feed } from '@pages';
+import { NotFound404 } from '@pages';
+import { Login } from '@pages';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Profile } from '@pages';
+import { ProfileOrders } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppHeader } from '@components';
+import { Modal } from '@components';
+import { OrderInfo } from '@components';
+import { IngredientDetails } from '@components';
+import { useDispatch } from '../../services/store';
+import { getIngredients } from '../../services/ingredientsSlice';
 
-import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Routes, Route } from 'react-router-dom';
-import { title } from 'process';
+const App = () => {
+  const dispatch = useDispatch();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Routes>
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
-      {/* защищенные маршруты отсюда */}
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/forgot-password' element={<ForgotPassword />} />
-      <Route path='/reset-password' element={<ResetPassword />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/profile/orders' element={<ProfileOrders />} />
-      <Route path='/profile/orders' element={<ProfileOrders />} />
-      {/* до сюда */}
-      {/* модальные окна с доп. информацией */}
-      <Route path='/feed/:number' element={<OrderInfo />} />
-      <Route path='/ingredients/:id' element={<IngredientDetails />} />
-      <Route path='/ingredients/:id' element={<IngredientDetails />} />
-      <Route path='/profile/orders/:number' element={<OrderInfo />} />
-      {/* до сюда */}
-      {/* страница ошибки */}
-      <Route path='*' element={<NotFound404 />} />
-    </Routes>
-  </div>
-);
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        {/* защищенные маршруты отсюда */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        {/* до сюда */}
+        {/* модальные окна с доп. информацией */}
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        {/* до сюда */}
+        {/* страница ошибки */}
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { ingredientsSlice } from './ingredientsSlice';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+export const rootReducer = combineReducers({
+  [ingredientsSlice.name]: ingredientsSlice.reducer
+});
 
-const store = configureStore({
+export const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
-
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useDispatch: () => AppDispatch = () => dispatchHook();
